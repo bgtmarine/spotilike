ab4bb733679eaf61723d0f3a33bdfc133e1a0fed
JulienTouchard <julien.touchard@free.fr>
2023-10-16 15:58:07 +0200

    next prev

--- assets/js/modules/slider.js
+++ assets/js/modules/slider.js
@@ -1,4 +1,4 @@
-const slider = (catalogue,currentTrack)=>{
+const slider = (catalogue, currentTrack, status = "init") => {
     console.log("initialisation du slider");
     /* console.dir(catalogue);
     console.log(catalogue[0].cover); */
@@ -6,19 +6,31 @@ const slider = (catalogue,currentTrack)=>{
     const sliderHTML = document.querySelector("#slider");
     const coverUrl = "./assets/img/cover/";
 
-    
-    const coverSlider = document.createElement("img");
-    coverSlider.src = coverUrl+catalogue[currentTrack].cover;
 
+    switch (status) {
+        case "init":
+            const coverSlider = document.createElement("img");
+            coverSlider.src = coverUrl + catalogue[currentTrack].cover;
+            coverSlider.id = "coverSlider";
+            // prepend insere un element avant ceux qui existent déjà dans le parent
+            //sliderHTML.prepend(coverSlider);
+            // append insere un element après ceux qui existent déjà dans le parent
+            sliderHTML.append(coverSlider);
+            break;
+        case "next":
+            document.querySelector("#coverSlider").src =
+                coverUrl + catalogue[currentTrack].cover;
+            break;
+        case "prev":
+            document.querySelector("#coverSlider").src =
+                coverUrl + catalogue[currentTrack].cover;
+            break;
+        default:
+            break;
+    }
 
-
-
-
-    // prepend insere un element avant ceux qui existent déjà dans le parent
-    //sliderHTML.prepend(coverSlider);
-    // append insere un element après ceux qui existent déjà dans le parent
-    sliderHTML.append(coverSlider);
 
 
 };
-export {slider};
+
+export { slider };
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,23 @@ const prevButton = document.querySelector("#prev");
 const nextButton = document.querySelector("#next");
 // click sur le bouton next
 nextButton.addEventListener("click",()=>{
-    if (currentTrack < catalogue.length) {
+    if (currentTrack < catalogue.length-1) {
         currentTrack++;
     } else {
         currentTrack = 0;
     }
+    slider(catalogue,currentTrack,"next");
     console.log(currentTrack);
 })
 // idem pour previous
-
+prevButton.addEventListener("click",()=>{
+    if (currentTrack > 0) {
+        currentTrack--;
+    } else {
+        currentTrack = catalogue.length-1;
+    }
+    slider(catalogue,currentTrack,"prev");
+    console.log(currentTrack);
+})
 
 slider(catalogue,currentTrack);
